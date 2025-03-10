c9bbb0e744895370484589607b9eff0c28b5864f
griezru <111182569+griezru@users.noreply.github.com>
2025-03-10 05:09:33 +0330

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,18 @@ def get_db():
 def index():
     return render_template('index.html')
 
-# ثبت نام کاربر
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data['username']
-    email = data['email']
-
-    # ذخیره اطلاعات کاربر در پایگاه داده
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO users (username, email) VALUES (?, ?)', (username, email))
-    conn.commit()
-
-    return jsonify({'success': True})
-
 # ثبت پیش‌بینی
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
+    user_id = data['userId']
     game1 = data['game1']
     game2 = data['game2']
 
-    # ثبت پیش‌بینی‌ها در پایگاه داده
+    # ذخیره پیش‌بینی‌ها در پایگاه داده
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO predictions (game1, game2) VALUES (?, ?)', (game1, game2))
+    cursor.execute('INSERT INTO predictions (user_id, game1, game2) VALUES (?, ?, ?)', (user_id, game1, game2))
     conn.commit()
 
     return jsonify({'success': True})
